903ba61def12a8cf6661edf10bb5dfa0e9a7ed4f
Craig Barkley <37020436+tektechnologies@users.noreply.github.com>
2024-10-16 03:21:40 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+firstName = input("What is your first name? ")
+lastName = input("What is your last name? ")
+favFood = input("What is your favorite food? ")
+favColor = input("What is your favorite color? ")
+travel = input("If you could travel anywhere in the world, where would you go? ")
+
+print("Hello " + firstName + " " + lastName + "! \nI see that your favorite food is " + favFood + " and your favorite color is " + favColor + ". It's cool that you would like to travel to " + travel + "!")
